7da84b4f6fa132f54106f32ffb546321d9f6e82f
asmismi2017-thv <asmismi2017@gmail.com>
2026-05-05 08:04:18 +0545

    command line arguments

---
+++ CommandLineArguments.java
@@ -0,0 +1,36 @@
+// public class CommandLineArguments {
+//     public static void main(String[] args) {
+//         System.out.println(x; "Hello");
+//         System.out.println(args.length);
+        
+//         }
+//     }
+
+//     //passing command line arguments to a java program
+//     //java .\C then shift c and tap. write first letter of that file name.
+
+
+//wap to print all arguments (command line) using for loop.
+
+public class CommandLineArguments {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+

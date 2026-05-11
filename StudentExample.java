5b6ee78b0a1c1c045a50e631bb7b28c38999e424
asmismi2017-thv <asmismi2017@gmail.com>
2026-05-11 07:51:42 +0545

    studentClass&Objectadded

---
+++ StudentExample.java
@@ -0,0 +1,8 @@
+public class StudentExample{
+    public static void main(String[]args){
+        Student s1 = new Student();
+        s1.name = "yun";
+        s1.rollno = 6;
+        System.out.println("name:" + s1.name + " rollno:" + s1.rollno);
+    }
+}
